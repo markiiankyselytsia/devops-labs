4343a34f9740e1617c4c4fa021330af6f71de7b2
OLeh <markiian.kyselytsia.itis.2018@lpnu.ua>
2021-01-15 23:39:05 +0200

    lab3

--- lab3/my_site/my_site/settings.py
+++ lab3/my_site/my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '89)gl(*ns909o6$!2*m^)^zf72x!kn$s51zb4x&z9pfxkvpi6y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
